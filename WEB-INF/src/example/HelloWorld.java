026f3e62ab27d9853cf298b07f7128d5c7c8a857
김영재 <poweryjkim@naver.com>
2023-09-06 01:46:01 +0000

    자바웹프로그래밍(2)

--- WEB-INF/src/example/HelloWorld.java
+++ WEB-INF/src/example/HelloWorld.java
@@ -2,6 +2,6 @@ package example;
 
 public class HelloWorld {
     public String hello() {
-        return "NICE! Hello World2";
+        return "NICE! Hello World2";    //코드 수정 후 서버 리부팅 필수, 리부팅하지 않으면 새로고침해도 업데이트가 되지 않음.
     }
 }
