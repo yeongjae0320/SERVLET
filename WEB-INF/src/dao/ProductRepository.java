b5076c632af7354c3da7d2b90f305ad4f910b9d7
김영재 <poweryjkim@naver.com>
2023-09-27 00:41:51 +0000

    자바웹프로그래밍(2)

--- WEB-INF/src/dao/ProductRepository.java
+++ WEB-INF/src/dao/ProductRepository.java
@@ -26,6 +26,48 @@ public class ProductRepository {
 		tablet.setManufacturer("Samsung");
 		tablet.setUnitsInStock(1000);
 		tablet.setCondition("Old");
+        
+        Product phone = new Product("P1234", "iPhone 6s", 800000);
+		phone.setDescription("4.7-inch, 1334X750 Renina HD display, 8-megapixel iSight Camera");
+		phone.setCategory("Smart Phone");
+		phone.setManufacturer("Apple");
+		phone.setUnitsInStock(1000);
+		phone.setCondition("New");
+
+        Product notebook = new Product("P1235", "LG PC 그램", 1500000);
+		notebook.setDescription("13.3-inch, IPS LED display, 5rd Generation Intel Core processors");
+		notebook.setCategory("Notebook");
+		notebook.setManufacturer("LG");
+		notebook.setUnitsInStock(1000);
+		notebook.setCondition("Refurbished");
+
+		Product tablet = new Product("P1236", "Galaxy Tab S", 900000);
+		tablet.setDescription("212.8*125.6*6.6mm,  Super AMOLED display, Octa-Core processor");
+		tablet.setCategory("Tablet");
+		tablet.setManufacturer("Samsung");
+		tablet.setUnitsInStock(1000);
+		tablet.setCondition("Old");
+        
+        Product phone = new Product("P1234", "iPhone 6s", 800000);
+		phone.setDescription("4.7-inch, 1334X750 Renina HD display, 8-megapixel iSight Camera");
+		phone.setCategory("Smart Phone");
+		phone.setManufacturer("Apple");
+		phone.setUnitsInStock(1000);
+		phone.setCondition("New");
+
+        Product notebook = new Product("P1235", "LG PC 그램", 1500000);
+		notebook.setDescription("13.3-inch, IPS LED display, 5rd Generation Intel Core processors");
+		notebook.setCategory("Notebook");
+		notebook.setManufacturer("LG");
+		notebook.setUnitsInStock(1000);
+		notebook.setCondition("Refurbished");
+
+		Product tablet = new Product("P1236", "Galaxy Tab S", 900000);
+		tablet.setDescription("212.8*125.6*6.6mm,  Super AMOLED display, Octa-Core processor");
+		tablet.setCategory("Tablet");
+		tablet.setManufacturer("Samsung");
+		tablet.setUnitsInStock(1000);
+		tablet.setCondition("Old");
 		
 		// 위와 같이 상품 초기화 하고 아래에 상품을 추가
 
