0011ee32187fd41731f849ef8b7cafbf0fa4e9c8
김영재 <poweryjkim@naver.com>
2023-10-04 11:15:09 +0000

    자바웹프로그래밍(2)

--- WEB-INF/src/example/ShopTime.java
+++ WEB-INF/src/example/ShopTime.java
@@ -1,19 +1,27 @@
 package example;
-import java.util.Date; // 날짜 관련 정보 얻기
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class ShopTime {
     public String timenow() {
-       Date day = new java.util.Date();
+        // 한국 표준시 설정
+        TimeZone kst = TimeZone.getTimeZone("Asia/Seoul");
+        Calendar calendar = Calendar.getInstance(kst);
+
         String am_pm;
-        int hour = day.getHours(); // 시간
-        int minute = day.getMinutes(); // 분
-        int second = day.getSeconds(); // 초
-        if (hour / 12 == 0) {
-            am_pm = "AM"; // 오전 판단
+        int hour = calendar.get(Calendar.HOUR_OF_DAY); // 24시간 형식으로 시간 가져오기
+        int minute = calendar.get(Calendar.MINUTE);
+        int second = calendar.get(Calendar.SECOND);
+
+        if (hour < 12) {
+            am_pm = "AM";
         } else {
-            am_pm = "PM"; // 오후 판단
-            hour = hour - 12;
+            am_pm = "PM";
+            if (hour != 12) { // 12 PM을 그대로 유지
+                hour -= 12;
+            }
         }
+
         String CT = hour + ":" + minute + ":" + second + " " + am_pm; // 출력 문자열 조합
         return CT; // 문자열 리턴
     }
